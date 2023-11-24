7bb4e0db92886bd5caa2343710671b3809706a14
Rishab49 <rishab.rishu49@gmail.com>
2023-11-24 11:40:16 +0530

    added try catch and promise to download operation

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -10,51 +10,60 @@ export function Download() {
   const location = useLocation();
   useEffect(() => location.setLocation(window.location.pathname), []);
   useEffect(() => {
-    let canvas = document.createElement("canvas");
-    let svg = document.querySelector("svg");
-    let foreignObject = document.querySelector("foreignObject");
-    let width = node.dimension.width;
-    let height = node.dimension.height;
-    if (svg && foreignObject) {
-      canvas.classList.add("hidden");
-      canvas.height = node.dimension.height * ratio;
-      canvas.width = node.dimension.width * ratio;
-      document.querySelector(".container")?.appendChild(canvas);
-      svg?.setAttribute("viewBox", `0 0 ${width} ${height}`);
-      svg?.setAttribute("height", String(height));
-      svg?.setAttribute("width", String(width));
-      foreignObject?.setAttribute("height", String(height));
-      foreignObject?.setAttribute("width", String(width));
-      node.elem && foreignObject?.appendChild(node.elem);
-      let data = createImage(svg);
-      let img = new Image();
-      img.src = data;
-      img.onload = function () {
-        let ctx = canvas.getContext("2d");
-        // console.log("ratio", ratio);
-        // console.log(img.width, img.height);
-        ctx?.drawImage(
-          img,
-          0,
-          0,
-          img.width,
-          img.height,
-          0,
-          0,
-          img.width * ratio,
-          img.height * ratio
-        );
-        let dataURL = canvas.toDataURL("image/jpeg");
-        setState("Downloading");
-        let anchor = document.createElement("a");
-        anchor.download = "snipit.jpeg";
-        anchor.href = dataURL;
-        // console.log(anchor);
-        anchor.click();
-        setState("Downloaded");
-      };
-      // console.log(data);
-    }
+    new Promise((res, rej) => {
+      try {
+        let canvas = document.createElement("canvas");
+        let svg = document.querySelector("svg");
+        let foreignObject = document.querySelector("foreignObject");
+        let width = node.dimension.width;
+        let height = node.dimension.height;
+        if (svg && foreignObject) {
+          canvas.classList.add("hidden");
+          canvas.height = node.dimension.height * ratio;
+          canvas.width = node.dimension.width * ratio;
+          document.querySelector(".container")?.appendChild(canvas);
+          svg?.setAttribute("viewBox", `0 0 ${width} ${height}`);
+          svg?.setAttribute("height", String(height));
+          svg?.setAttribute("width", String(width));
+          foreignObject?.setAttribute("height", String(height));
+          foreignObject?.setAttribute("width", String(width));
+          node.elem && foreignObject?.appendChild(node.elem);
+          let data = createImage(svg);
+          let img = new Image();
+          img.src = data;
+          img.onload = function () {
+            let ctx = canvas.getContext("2d");
+            // console.log("ratio", ratio);
+            // console.log(img.width, img.height);
+            ctx?.drawImage(
+              img,
+              0,
+              0,
+              img.width,
+              img.height,
+              0,
+              0,
+              img.width * ratio,
+              img.height * ratio
+            );
+            let dataURL = canvas.toDataURL("image/jpeg");
+            setState("Downloading");
+            let anchor = document.createElement("a");
+            anchor.download = "snipit.jpeg";
+            anchor.href = dataURL;
+            // console.log(anchor);
+            anchor.click();
+            setState("Downloaded");
+          };
+          // console.log(data);
+        }
+        res("done");
+      } catch (e) {
+        console.log(e);
+        rej(e);
+        alert("error occured please try again")
+      }
+    });
   }, []);
 
   return (
@@ -68,9 +77,11 @@ export function Download() {
           <div className="p-4 rounded-md border-2 border-white bg-gray-600">
             Downloading...
           </div>
-        ) : <div className="p-4 rounded-md border-2 border-white bg-gray-600">
-        Downloaded
-      </div>}
+        ) : (
+          <div className="p-4 rounded-md border-2 border-white bg-gray-600">
+            Downloaded
+          </div>
+        )}
       </div>
       <svg>
         <foreignObject x="0" y="0"></foreignObject>
